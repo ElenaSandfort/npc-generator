68ec453f9512b94d9425a698b85184d540b1219f
Elena Sandfort <sandfort.elena@gmail.com>
2026-06-14 15:20:09 +0200

    Test random generated logic in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import './App.css'
+import './App.css';
+import { generateNPC } from './utils/generateNPC';
 
 function App() {
+  const npc = generateNPC();
   return (
     <>
-    <h1>NPC Generator</h1>
-    <p>App setup complete.</p>
+      <h1>NPC Generator</h1>
+      <h2>{npc.name}</h2>
+      <p>Gender: {npc.gender}</p>
+      <p>Species: {npc.species}</p>
+      <p>Profession: {npc.profession}</p>
+      <p>Alignment: {npc.alignment}</p>
+      <p>Quirk: {npc.quirk}</p>
+      <h3>Strenght: {npc.statBlock.abilityScores.strength}</h3>
+    
 
     </>
   )
